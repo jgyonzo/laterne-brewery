0366b2c07a50770141f167a9f97c3ccbd2112c3a
jgyonzo <juan.yonzo@mercadolibre.com>
2017-05-09 01:49:42 -0300

    add sensors

--- daemon/temperature_daemon.py
+++ daemon/temperature_daemon.py
@@ -11,6 +11,11 @@ output_pin_2 = 12 #Valvula sensor 2. GPIO18
 output_pin_3 = 11 #Valvula sensor 3. GPIO17
 output_pin_4 = 13 #Valvula sensor 4. GPIO27
 output_pin_5 = 15 #Valvula sensor 5. GPIO22
+output_pin_6 = 29  #Valvula sensor 6. GPIO5
+output_pin_7 = 31 #Valvula sensor 7. GPIO6
+output_pin_8 = 33 #Valvula sensor 8. GPIO13
+#output_pin_9 =  #AMBIENT. No usa
+#output_pin_10 =  #
 
 output_pin_banco_frio = 36 #Rele banco de frio. GPIO16
 output_pin_camara_frio = 32 #Rele camara de frio. GPIO12
@@ -22,6 +27,11 @@ temp_sens_2 = 21 #FV2
 temp_sens_3 = 1 #BBT1
 temp_sens_4 = 5 #BBT2
 temp_sens_5 = 23 #FV3
+temp_sens_6 = 21 #FV4 temp8
+temp_sens_7 = 21 #FV5 temp9
+temp_sens_8 = 21 #FV6 temp10
+#temp_sens_9 = 0 #AMBIENT temp11. No usa, solo guarda
+#temp_sens_10 = 0 #disponible temp12
 
 temp_sens_banco_frio = 1
 temp_sens_camara_frio = 10
@@ -37,6 +47,11 @@ GPIO.setup(output_pin_2, GPIO.OUT)
 GPIO.setup(output_pin_3, GPIO.OUT)
 GPIO.setup(output_pin_4, GPIO.OUT)
 GPIO.setup(output_pin_5, GPIO.OUT)
+GPIO.setup(output_pin_6, GPIO.OUT)
+GPIO.setup(output_pin_7, GPIO.OUT)
+GPIO.setup(output_pin_8, GPIO.OUT)
+#GPIO.setup(output_pin_9, GPIO.OUT)
+#GPIO.setup(output_pin_10, GPIO.OUT)
 GPIO.setup(output_pin_banco_frio, GPIO.OUT)
 GPIO.setup(output_pin_camara_frio, GPIO.OUT)
 GPIO.setup(pump_pin, GPIO.OUT)
@@ -45,6 +60,11 @@ GPIO.output(output_pin_2, True)
 GPIO.output(output_pin_3, True)
 GPIO.output(output_pin_4, True)
 GPIO.output(output_pin_5, True)
+GPIO.output(output_pin_6, True)
+GPIO.output(output_pin_7, True)
+GPIO.output(output_pin_8, True)
+#GPIO.output(output_pin_9, True)
+#GPIO.output(output_pin_10, True)
 GPIO.output(output_pin_banco_frio, True)
 GPIO.output(output_pin_camara_frio, True)
 GPIO.output(pump_pin, True)
@@ -72,6 +92,26 @@ try:
 except Exception as e:
     print("ERROR READING SENSOR 5")
 try:
+    sensor6 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "")
+except Exception as e:
+    print("ERROR READING SENSOR 6")
+try:
+    sensor7 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "")
+except Exception as e:
+    print("ERROR READING SENSOR 7")
+try:
+    sensor8 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "")
+except Exception as e:
+    print("ERROR READING SENSOR 8")
+try:
+    sensor9 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "")
+except Exception as e:
+    print("ERROR READING SENSOR 9")
+# try:
+#     sensor10 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "")
+# except Exception as e:
+#     print("ERROR READING SENSOR 10")
+try:
     sensorBancoFrio = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "0215c23d9eff")
 except Exception as e:
     print("ERROR READING SENSOR Banco Frio")
@@ -88,6 +128,11 @@ while True:
         sensor3_temp = 0.0
         sensor4_temp = 0.0
         sensor5_temp = 0.0
+        sensor6_temp = 0.0
+        sensor7_temp = 0.0
+        sensor8_temp = 0.0
+        sensor9_temp = 0.0
+        # sensor10_temp = 0.0
         sensorBancoFrio_temp = 0.0
         sensorCamaraFrio_temp = 0.0
         output_1_val = True
@@ -95,6 +140,11 @@ while True:
         output_3_val = True
         output_4_val = True
         output_5_val = True
+        output_6_val = True
+        output_7_val = True
+        output_8_val = True
+#        output_9_val = True
+        # output_10_val = True
         output_banco_frio_val = True
         output_camara_frio_val = True
         output_pump_val = True
@@ -126,6 +176,31 @@ while True:
             print("ERROR READING SENSOR 5")
 
         try:
+            sensor6_temp = sensor6.get_temperature()
+        except:
+            print("ERROR READING SENSOR 6")
+
+        try:
+            sensor7_temp = sensor7.get_temperature()
+        except:
+            print("ERROR READING SENSOR 7")
+
+        try:    
+            sensor8_temp = sensor8.get_temperature()
+        except:
+            print("ERROR READING SENSOR 8")
+
+        try:
+            sensor9_temp = sensor9.get_temperature()
+        except:
+            print("ERROR READING SENSOR 9")
+
+        # try:
+        #     sensor10_temp = sensor10.get_temperature()
+        # except:
+        #     print("ERROR READING SENSOR 10")
+
+        try:
             sensorBancoFrio_temp = sensorBancoFrio.get_temperature()
         except:
             print("ERROR READING SENSOR Banco Frio")
@@ -140,6 +215,11 @@ while True:
         print("Sensor 3 temp %.2f" % sensor3_temp)
         print("Sensor 4 temp %.2f" % sensor4_temp)
         print("Sensor 5 temp %.2f" % sensor5_temp)
+        print("Sensor 6 temp %.2f" % sensor6_temp)
+        print("Sensor 7 temp %.2f" % sensor7_temp)
+        print("Sensor 8 temp %.2f" % sensor8_temp)
+        print("Sensor 9 temp %.2f" % sensor9_temp)
+        # print("Sensor 10 temp %.2f" % sensor10_temp)
         print("Sensor Banco Frio temp %.2f" % sensorBancoFrio_temp)
         print("Sensor Camara Frio temp %.2f" % sensorCamaraFrio_temp)
 
@@ -148,6 +228,11 @@ while True:
         output_3_val = False if (sensor3_temp >= (temp_sens_3 + tolerancia)) else True if (sensor3_temp <= (temp_sens_3 - tolerancia)) else output_3_val
         output_4_val = False if (sensor4_temp >= (temp_sens_4 + tolerancia)) else True if (sensor4_temp <= (temp_sens_4 - tolerancia)) else output_4_val
         output_5_val = False if (sensor5_temp >= (temp_sens_5 + tolerancia)) else True if (sensor5_temp <= (temp_sens_5 - tolerancia)) else output_5_val
+        output_6_val = False if (sensor6_temp >= (temp_sens_6 + tolerancia)) else True if (sensor6_temp <= (temp_sens_6 - tolerancia)) else output_6_val
+        output_7_val = False if (sensor7_temp >= (temp_sens_7 + tolerancia)) else True if (sensor7_temp <= (temp_sens_7 - tolerancia)) else output_7_val
+        output_8_val = False if (sensor8_temp >= (temp_sens_8 + tolerancia)) else True if (sensor8_temp <= (temp_sens_8 - tolerancia)) else output_8_val
+#        output_9_val = False if (sensor9_temp >= (temp_sens_9 + tolerancia)) else True if (sensor9_temp <= (temp_sens_9 - tolerancia)) else output_9_val
+        # output_10_val = False if (sensor10_temp >= (temp_sens_10 + tolerancia)) else True if (sensor10_temp <= (temp_sens_10 - tolerancia)) else output_10_val
         output_banco_frio_val = False if (sensorBancoFrio_temp >= (temp_sens_banco_frio + tolerancia)) else True if (sensorBancoFrio_temp <= (temp_sens_banco_frio - tolerancia)) else output_banco_frio_val
         output_camara_frio_val = False if (sensorCamaraFrio_temp >= (temp_sens_camara_frio + tolerancia)) else True if (sensorCamaraFrio_temp <= (temp_sens_camara_frio - tolerancia)) else output_camara_frio_val
 
@@ -158,6 +243,11 @@ while True:
         GPIO.output(output_pin_3, output_3_val)
         GPIO.output(output_pin_4, output_4_val)
         GPIO.output(output_pin_5, output_5_val)
+        GPIO.output(output_pin_6, output_6_val)
+        GPIO.output(output_pin_7, output_7_val)
+        GPIO.output(output_pin_8, output_8_val)
+#        GPIO.output(output_pin_9, output_9_val)
+        # GPIO.output(output_pin_10, output_10_val)
         GPIO.output(output_pin_banco_frio, output_banco_frio_val)
         GPIO.output(output_pin_camara_frio, output_camara_frio_val)
         GPIO.output(pump_pin, output_pump_val)
@@ -167,6 +257,11 @@ while True:
         print("Output 3 on? --> ", not output_3_val)
         print("Output 4 on? --> ", not output_4_val)
         print("Output 5 on? --> ", not output_5_val)
+        print("Output 6 on? --> ", not output_6_val)
+        print("Output 7 on? --> ", not output_7_val)
+        print("Output 8 on? --> ", not output_8_val)
+#        print("Output 9 on? --> ", not output_9_val)
+        # print("Output 10 on? --> ", not output_10_val)
         print("Output Banco Frio on? --> ", not output_banco_frio_val)
         print("Output Camara Frio on? --> ", not output_camara_frio_val)
         print("Pump on? --> ", not output_pump_val)
@@ -182,11 +277,11 @@ while True:
         try:
             with connection.cursor() as cursor:
                 # Create a new record
-                #cursor.execute("""INSERT INTO temps (temp1,temp2) VALUES (%s,%s) """,(sensor1_temp,sensor2_temp))
-                cursor.execute("""UPDATE temps4 SET temp1 = %s,temp2 = %s, temp3 = %s, temp4 = %s, temp5 = %s, temp6 = %s, temp7 = %s, temp1_cfg = %s, temp2_cfg = %s, temp3_cfg = %s, temp4_cfg = %s, temp5_cfg = %s, temp6_cfg = %s, temp7_cfg = %s, output1 = %s, output2 = %s, output3 = %s, output4 = %s, output5 = %s, output6 = %s, output7 = %s WHERE 1=1 """,
-                                                (sensor1_temp,sensor2_temp,sensor3_temp,sensor4_temp,sensor5_temp,sensorBancoFrio_temp,sensorCamaraFrio_temp,
-                                                   temp_sens_1,temp_sens_2,temp_sens_3,temp_sens_4,temp_sens_5,temp_sens_banco_frio,temp_sens_camara_frio,
-                                                   output_1_val, output_2_val, output_3_val, output_4_val, output_5_val, output_banco_frio_val, output_pin_camara_frio
+                #temp6 y temp7 son banco de frio y camara de frio. El de ambiente (temp9) no lleva cfg ni output
+                cursor.execute("""UPDATE temps4 SET temp1 = %s,temp2 = %s, temp3 = %s, temp4 = %s, temp5 = %s, temp6 = %s, temp7 = %s, temp8 = %s,temp9 = %s, temp10 = %s, temp11 = %s, temp12 = %s, temp1_cfg = %s, temp2_cfg = %s, temp3_cfg = %s, temp4_cfg = %s, temp5_cfg = %s, temp6_cfg = %s, temp7_cfg = %s, temp8_cfg = %s, temp9_cfg = %s, temp10_cfg = %s, temp11_cfg = %s, temp12_cfg = %s, output1 = %s, output2 = %s, output3 = %s, output4 = %s, output5 = %s, output6 = %s, output7 = %s, output8 = %s, output9 = %s, output10 = %s, output11 = %s, output12 = %s WHERE 1=1 """,
+                                                (sensor1_temp,sensor2_temp,sensor3_temp,sensor4_temp,sensor5_temp,sensorBancoFrio_temp,sensorCamaraFrio_temp,sensor6_temp,sensor7_temp,sensor8_temp,sensor9_temp,0,
+                                                   temp_sens_1,temp_sens_2,temp_sens_3,temp_sens_4,temp_sens_5,temp_sens_banco_frio,temp_sens_camara_frio,temp_sens_6,temp_sens_7,temp_sens_8,0,0,
+                                                   output_1_val, output_2_val, output_3_val, output_4_val, output_5_val, output_banco_frio_val, output_pin_camara_frio,output_6_val, output_7_val, output_8_val, False, False
                                                    ))
 
             connection.commit()
--- scripts/database.sql
+++ scripts/database.sql
@@ -7,3 +7,5 @@ CREATE TABLE temps4 ( temp1 FLOAT, temp2 FLOAT, temp3 FLOAT, temp4 FLOAT, temp5 
 insert into temps4 (temp1,temp2,temp3,temp4,temp5) values (3.5,3.5,3.5,3.5,3.5);
 
 alter table temps4 ADD temp6 FLOAT, ADD temp7 FLOAT, ADD temp8 FLOAT, ADD temp6_cfg FLOAT, ADD temp7_cfg FLOAT, ADD temp8_cfg FLOAT, ADD output6 INTEGER, ADD output7 INTEGER, ADD output8 INTEGER;
+
+alter table temps4 ADD temp9 FLOAT, ADD temp10 FLOAT, ADD temp9_cfg FLOAT, ADD temp10_cfg FLOAT, ADD output9 INTEGER, ADD output10 INTEGER;
